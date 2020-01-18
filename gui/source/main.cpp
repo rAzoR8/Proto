f3a3c882e713945031dd1af7606f60c9d906fbec
Fabian Wahlster <fwahlster@outlook.com>
2020-01-18 11:40:38 +0100

    add imgui sources to proto

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,5 +1,21 @@
+#include "imgui.h"
 
 int main(int argc, char* argv[])
 {
+	//IMGUI_CHECKVERSION();
+	//ImGuiContext* pContext = ImGui::CreateContext();
+
+	//ImGui::Text("Hello, world %d", 123);
+	//if (ImGui::Button("Save")) []() {};
+
+	//char buf[256];
+	//float f = 3.f;
+	//ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+	//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
+	//ImGui::Render();
+
+	//ImGui::DestroyContext();
+
 	return 0;
 }
